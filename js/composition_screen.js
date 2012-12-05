4c143f8af73fd6e175ea098527d55d23ba7f10eb
Krista Purser <clever_curls@hotmail.com>
2012-12-05 11:16:07 -0700

    adding playback buttons in composition

--- js/composition_screen.js
+++ js/composition_screen.js
@@ -11,3 +11,23 @@ function appendElements(){
 	comp.innerHTML=temp;
 }
 
+		function playPressed(){
+			var text = "<img src=\"../imgs/button_pause.png\" height =\"50%\" onClick ='pausePressed()'> "
+			text += " <img src=\"../imgs/button_stop_brown.png\" height =\"50%\" onClick =\"stopPressed()\">"
+            text += " <img src=\"../imgs/button_rewind.png\" height =\"50%\">"
+            text += " <img src=\"../imgs/button_forward.png\" height =\"50%\">"
+			document.getElementById("playback").innerHTML = text;
+		}
+		function stopPressed(){
+			var text = " <img src=\"../imgs/button_play.png\" height =\"50%\" onClick =\"playPressed()\">"
+			document.getElementById("playback").innerHTML = text;
+		}
+		
+		function pausePressed(){
+			var text = "<img src=\"../imgs/button_play_brown.png\" height =\"50%\" onClick ='playPressed()'> "
+			text += " <img src=\"../imgs/button_stop_brown.png\" height =\"50%\" onClick =\"stopPressed()\">"
+            text += " <img src=\"../imgs/button_rewind.png\" height =\"50%\">"
+            text += " <img src=\"../imgs/button_forward.png\" height =\"50%\">"
+			document.getElementById("playback").innerHTML = text;
+		}
+

febf278fe4c4f4ef21cb3c1c4e3ead0d77422e90
Christopher Heuer <cheuer@gmail.com>
2012-12-05 13:49:47 -0700

    setting note values

--- js/composition_screen.js
+++ js/composition_screen.js
@@ -11,7 +11,7 @@ function insertElements(){
 function appendElements(){	
 	var comp = document.getElementById('composition');
 	var newdiv = document.createElement('div');
-	newdiv.innerHTML='<a onclick="insertElements();"><img class="note" src="../imgs/Cnew.PNG" alt="" height /></a>';
+	newdiv.innerHTML='<span class="note" onclick="insertElements();"></span>';
 	var temp = comp.innerHTML+newdiv.innerHTML;
 	comp.innerHTML=temp;
 }
